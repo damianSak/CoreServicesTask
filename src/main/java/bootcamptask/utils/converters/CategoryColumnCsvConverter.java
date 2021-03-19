c060ed253a200e05abe8a545f77cee62da321206
damianSak <damian.sak88@gmail.com>
2021-03-19 03:35:03 +0100

    Changeg method to public, otherwise openCSV not working

--- src/main/java/bootcamptask/utils/converters/CategoryColumnCsvConverter.java
+++ src/main/java/bootcamptask/utils/converters/CategoryColumnCsvConverter.java
@@ -5,7 +5,7 @@ import com.opencsv.bean.AbstractBeanField;
 
 public class CategoryColumnCsvConverter<T, I> extends AbstractBeanField<T, I> {
 
-    CategoryColumnCsvConverter() {}
+    public CategoryColumnCsvConverter() {}
 
     @Override
     protected Object convert(String valueFromCsv) {
